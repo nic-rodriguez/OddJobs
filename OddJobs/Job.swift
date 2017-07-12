9afb2e2d77b078f2c7f66cd15d343a29ba1c1456
Nicolas Rodriguez <nhr@fb.com>
2017-07-12 00:15:36 -0700

    Added files for job object, added classes for new view controllers

--- OddJobs/Job.swift
+++ OddJobs/Job.swift
@@ -7,7 +7,47 @@
 //
 
 import UIKit
+import Parse
 
 class Job: NSObject {
-
+    
+//    private var userAccepted: User?
+//    private var usersInterested: [User]?
+//    private var id: Int64
+    
+    /* important user vars
+     
+     initialize user vars with user["location"] syntax
+     reference Instagram for example code/syntax
+     
+     location: String //Google maps type
+     name: String
+     username: String
+     bio: String?
+     profilePic: URL?
+     jobsTaken: [Job]?
+     jobsPosted: [Job]?
+     currentJob: Job?
+     rating: Double
+     bookmarkedUsers: [PFUser: Bool]?
+     tags: [String]?
+    */
+    
+    class func postJob(location: String, title: String, description: String, datePosted: Date, dateDue: Date, tags: [String]?, difficulty: Int, pay: Double, image: URL?, completion: PFBooleanResultBlock?) {
+        
+        let job = PFObject(className: "Job")
+        
+        job["location"] = location
+        job["title"] = title
+        job["description"] = description
+        job["datePosted"] = datePosted
+        job["dateDue"] = dateDue
+        job["tags"] = tags
+        job["difficulty"] = difficulty
+        job["pay"] = pay
+        job["image"] = image
+        job["userPosted"] = PFUser.current()
+        job["isAvailable"] = true
+        
+    }
 }
