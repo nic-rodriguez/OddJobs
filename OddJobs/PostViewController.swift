28157a60aba7c63ffcc86289973c89ac5be30947
Pavani Malli <pavanim@fb.com>
2017-07-12 12:06:11 -0700

    Added UI elements for postViewController

--- OddJobs/PostViewController.swift
+++ OddJobs/PostViewController.swift
@@ -6,30 +6,72 @@
 //  Copyright © 2017 Nicolas Rodriguez. All rights reserved.
 //
 
+//TODO: GPS location, place autocomplete, tableView for tags
+
 import UIKit
 
 class PostViewController: UIViewController {
 
+    
+    @IBOutlet weak var jobTitleField: UITextField!
+    
+    @IBOutlet weak var jobDescriptionField: UITextField!
+    
+    @IBOutlet weak var addressField: UITextField!
+    
+    @IBOutlet weak var payField: UITextField!
+    
+    @IBOutlet weak var jobDatePicker: UIDatePicker!
+    
+    
+    var jobTitle: String = ""
+    var jobDescription: String = ""
+    var pay: Double = 0
+    var jobDate: Date
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+    
+    
+    
     }
     
 
-    /*
-    // MARK: - Navigation
+    
+    @IBAction func postJob(_ sender: UIButton) {
+        jobTitle = jobTitleField.text!
+        jobDescription = jobDescriptionField.text!
+        pay = Double(payField.text!) ?? 0
+        jobDate = jobDatePicker.date
+        
+        
+        
+        
+        Job.postJob(location: <#T##String#>, title: jobTitle, description: jobDescription, datePosted: <#T##Date#>, dateDue: jobDate, tags: <#T##[String]?#>, difficulty: 0, pay: pay, image: URL?, completion: { (success, error) in
+            if success {
+                print("Post was saved!")
+                
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        })
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        
+        
+        }
+        
+
 
 }
