f2acbe031204e5453dc9fc35b5c05154b9f410e4
Nicolas Rodriguez <nhr@fb.com>
2017-07-12 11:04:07 -0700

    Added didSet property of job inside HomeFeedTableViewCell

--- OddJobs/HomeFeedTableViewCell.swift
+++ OddJobs/HomeFeedTableViewCell.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
+import Parse
+import ParseUI
 
 class HomeFeedTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var postImageView: PFImageView!
+    @IBOutlet weak var userImageView: PFImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    var job: PFObject! {
+        didSet {
+            self.postImageView.file = job["image"] as? PFFile
+            self.postImageView.loadInBackground()
+            
+            let jobPoster = job["userPosted"] as! PFUser
+            self.userImageView.file = jobPoster["profilePic"] as? PFFile
+            self.userImageView.loadInBackground()
+            
+            self.nameLabel.text = jobPoster["name"] as? String
+            self.titleLabel.text = job["title"] as? String
+            self.dateLabel.text = job["dateDue"] as? String
+            self.costLabel.text = job["pay"] as? String
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
