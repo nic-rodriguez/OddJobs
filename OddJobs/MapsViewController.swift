19ca7c9763146c47af972d35fead770623a295ab
Nicolas Rodriguez <nhr@fb.com>
2017-07-18 10:53:29 -0700

    fixed issue where map kept centering on every update of user location

--- OddJobs/MapsViewController.swift
+++ OddJobs/MapsViewController.swift
@@ -17,6 +17,7 @@ class MapsViewController: UIViewController {
     let regionRadius: CLLocationDistance = 1000
     let locationManager = CLLocationManager()
     var jobs: [PFObject] = []
+    var initalLocation: MKUserLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +30,9 @@ class MapsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
     }
     
     func requestLocationAccess() {
@@ -51,6 +55,17 @@ class MapsViewController: UIViewController {
         mapView.setRegion(coordinateRegion, animated: true)
     }
     
+    func createAnnotations(jobs: [PFObject], map: MKMapView) {
+        map.addAnnotation(map.userLocation)
+        for job in jobs {
+            let latitude = job["latitude"] as! CLLocationDegrees
+            let longitude = job["longitude"] as! CLLocationDegrees
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let annotation = Job(title: job["title"] as? String, subtitle: job["description"] as? String, location: coordinate)
+            map.addAnnotation(annotation)
+        }
+    }
+    
     func queryServer() {
         let query = PFQuery(className: "Job")
         query.addDescendingOrder("createdAt")
@@ -62,15 +77,8 @@ class MapsViewController: UIViewController {
             if let error = error {
                 print(error.localizedDescription)
             } else {
-                print("query worked")
                 self.jobs = jobs!
-                for job in self.jobs {
-                    let latitude = job["latitude"] as! CLLocationDegrees
-                    let longitude = job["longitude"] as! CLLocationDegrees
-                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-                    let annotation = Job(title: job["title"] as? String, subtitle: job["description"] as? String, location: coordinate)
-                    self.mapView.addAnnotation(annotation)
-                }
+                self.createAnnotations(jobs: self.jobs, map: self.mapView)
             }
         }
     }
@@ -80,14 +88,10 @@ class MapsViewController: UIViewController {
 extension MapsViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        if let userLocation = mapView.userLocation.location {
-            centerMapOnLocation(location: userLocation)
+        if initalLocation == nil {
+            initalLocation = userLocation
+            centerMapOnLocation(location: userLocation.location!)
         }
     }
     
-//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-//        print("annotation view")
-//        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
-//        return annotationView
-//    }
 }
