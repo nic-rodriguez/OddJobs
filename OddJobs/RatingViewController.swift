bc267d7379a10f8dc1541153934210c601361989
Nicolas Rodriguez <nhr@fb.com>
2017-07-25 17:00:57 -0700

    added didSet properties of variables, and actions to be completed upon tapping done

--- OddJobs/RatingViewController.swift
+++ OddJobs/RatingViewController.swift
@@ -19,7 +19,19 @@ class RatingViewController: UIViewController {
     @IBOutlet weak var fiveStar: UIButton!
     @IBOutlet weak var userLabel: UILabel!
     @IBOutlet weak var jobLabel: UILabel!
-    @IBOutlet weak var jobImageView: UIImageView!
+    @IBOutlet weak var userImageView: PFImageView!
+    @IBOutlet weak var commentsTextField: UITextField!
+    
+    var user: PFUser!
+    var job: PFObject! {
+        didSet {
+            user = job["userPosted"] as? PFUser
+            userImageView.file = user["profilePicture"] as? PFFile
+            userImageView.loadInBackground()
+            jobLabel.text = job["title"] as? String
+        }
+    }
+    var ratingSelected = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +40,39 @@ class RatingViewController: UIViewController {
     }
 
     @IBAction func donePress(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        var comments = user["comments"] as? [String]
+        var rating = user["rating"] as? [Int]
+        
+        if rating == nil {
+            rating = []
+        }
+        if comments == nil {
+            comments = []
+        }
+        
+        if let comment = commentsTextField.text {
+            comments!.append(comment)
+            user["comments"] = comments!
+        }
+        
+        if fiveStar.isSelected {
+            rating!.append(5)
+        } else if fourStar.isSelected {
+            rating!.append(4)
+        } else if threeStar.isSelected {
+            rating!.append(3)
+        } else if twoStar.isSelected {
+            rating!.append(2)
+        } else if oneStar.isSelected {
+            rating!.append(1)
+        } else {
+            //raise alert controller: please select a rating
+            ratingSelected = false
+        }
+        if ratingSelected {
+            user["rating"] = rating
+            user.saveInBackground()
+            dismiss(animated: true, completion: nil)
+        }
     }
 }
