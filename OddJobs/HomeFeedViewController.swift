d5fd071333cf82497768f7efe62b0ae504819384
Pavani Malli <pavanim@fb.com>
2017-08-04 10:48:18 -0700

    Font change

--- OddJobs/HomeFeedViewController.swift
+++ OddJobs/HomeFeedViewController.swift
@@ -232,8 +232,8 @@ class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableView
     
     func configureCustomSearchController() {
         //customSearchController = CustomSearchController(searchResultsController: self, searchBarFrame: CGRect.init(x: 0.0, y: 0.0, width: homeFeedTableView.frame.size.width, height: 50.0), searchBarFont: UIFont(name: "Futura", size: 16.0)!, searchBarTextColor: UIColor.orange, searchBarTintColor: UIColor.black)
-        customSearchController = CustomSearchController(searchResultsController: self, searchBarFrame: CGRect.init(x: 0.0, y: 0.0, width: homeFeedTableView.frame.size.width, height: 40.0), searchBarFont: UIFont(name: "HelveticaNeue", size: 15.0)!, searchBarTextColor: color.myTealColor, searchBarTintColor: color.myLightColor)
-        customSearchController.customSearchBar.placeholder = "Search"
+        customSearchController = CustomSearchController(searchResultsController: self, searchBarFrame: CGRect.init(x: 0.0, y: 0.0, width: homeFeedTableView.frame.size.width, height: 40.0), searchBarFont: UIFont(name: "HelveticaNeue-Thin", size: 14.0)!, searchBarTextColor: color.myTealColor, searchBarTintColor: color.myLightColor)
+        customSearchController.customSearchBar.placeholder = "          Search"
         homeFeedTableView.tableHeaderView = customSearchController.customSearchBar
         customSearchController.customDelegate = self
     }
